710a33d5e37588a74010a64fb9748411d07b035c
Tusjine <justmeh112299@gmail.com>
2023-04-06 09:48:14 +0800

    Validate task id

--- resources/views/livewire/task-comment-section.blade.php
+++ resources/views/livewire/task-comment-section.blade.php
@@ -1,7 +1,7 @@
 <div class="p-6 lg:p-8 flex flex-col space-y-5">
     {{-- {{ route('comment.store', $task) }} --}}
     <form wire:submit.prevent="addComment">
-        <div class="my-5">
+        <div class="mb-5">
             <label for="comment"> Add Comment</label>
             <textarea wire:model="comment" name="comment" id="comment" rows="3" class="mt-1 block w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"></textarea>
             @error('comment') <x-input-error for="comment" class="mt-2" /> @enderror
